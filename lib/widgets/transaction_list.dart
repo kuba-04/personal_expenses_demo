aea43b952d1c6901d6106ef177f8cb6850c39201
kuba-04 <jakub.gladysz1@gmail.com>
2020-04-14 21:01:59 +0200

    ListView builder for more efficient list rendering

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,52 +3,58 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  final List<Transaction> userTransactions;
+  final List<Transaction> transactions;
 
-  TransactionList(this.userTransactions);
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-              children: userTransactions.map((tx) {
+    return Container(
+      height: 300,
+      child: ListView.builder(
+          itemBuilder: (ctx, index) {
             return Card(
-                child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 20,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2)),
-                  padding: EdgeInsets.all(10),
-                  child: Text('\$${tx.amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      )),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                      textAlign: TextAlign.start,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 15,
                     ),
-                    Text(
-                      DateFormat('yyyy-MM-dd').format(tx.date),
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                )
-              ],
-            ),);
-          }).toList()
-        );
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text('\$${transactions[index].amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        )),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                      Text(
+                        DateFormat('yyyy-MM-dd').format(transactions[index].date),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: transactions.length,)
+          // children: transactions.map((tx) {
+          // }).toList()),
+    );
   }
 }
